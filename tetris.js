8b28bdf218ed573a09f188f315abeb1f78d3e3a4
Lordgg007 <supergaudy1@gmail.com>
2023-09-04 23:28:57 -0400

    fix score

--- tetris.js
+++ tetris.js
@@ -5,7 +5,7 @@ context.scale(20, 20);
 
 function arenaSweep() {
 	let rowCount = 1;
-	outer: for (let y = arena.length - 1; y < 0; --y) {
+	outer: for (let y = arena.length -1; y > 0; --y) {
 		for (let x = 0; x < arena[y].length; ++x) {
 			if (arena[y][x] === 0) {
 				continue outer;
@@ -13,7 +13,7 @@ function arenaSweep() {
 		}
 		const row = arena.splice(y, 1)[0].fill(0);
 		arena.unshift(row);
-		++y
+		++y;
 
 		player.score += rowCount * 10;
 		rowCount *= 2;
@@ -23,15 +23,16 @@ function arenaSweep() {
 
 //Collision Detection
 function collide(arena, player) {
-	const [m, o] = [player.matrix, player.pos];
+	const m = player.matrix;
+	const o = player.pos;
 	for (let y = 0; y < m.length; ++y) {
-		for (let x = 0; x < m[y].length; ++x) {
-			if (m[y][x] !== 0 &&
-					(arena[y + o.y] &&
-					arena[y + o.y][x + o.x]) !== 0) {
-					return true;
+			for (let x = 0; x < m[y].length; ++x) {
+					if (m[y][x] !== 0 &&
+						(arena[y + o.y] &&
+							arena[y + o.y][x + o.x]) !== 0) {
+							return true;
+					}
 			}
-		}
 	}
 	return false;
 }
